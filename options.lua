09bd7e41f44b5561ec45dfb2fd22aef3a2fcbb3a
hankrof <hankrof@gmail.com>
2026-01-03 01:27:38 +0800

    migrate init.vim to init.lua
    split init.vim into 3 files
      a.) options.lua
      b.) keymaps.lua
      c.) autocmds.lua

---
+++ options.lua
@@ -0,0 +1,17 @@
+-- configuring options
+local set=vim.opt
+local cmd=vim.cmd
+
+set.number = true
+set.autoindent = true
+set.tabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
+set.hlsearch = true
+set.incsearch = true
+set.updatetime = 150
+set.list = true
+set.mouse = ""
+set.termguicolors = true
+cmd("colorscheme industry")
+cmd("syntax on")
