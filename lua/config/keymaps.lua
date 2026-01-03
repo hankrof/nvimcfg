2a2f052095e4976b885303c42018a74c71516959
hankrof <hankrof@gmail.com>
2026-01-03 19:45:18 +0800

    Bind <F9> to neotree toggle

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,5 @@
 local keymap = vim.keymap.set
-keymap("n", "<F9>", "<Esc>:Neotree<CR>", { silent = true })
+keymap("n", "<F9>", "<Esc>:Neotree toggle<CR>", { silent = true })
 keymap("n", "<F8>", "<Esc>:CocCommand document.toggleInlayHint<CR>", { silent = true })
 keymap("n", "q:", "<nop>")
 keymap("n", "q/", "<nop>")
